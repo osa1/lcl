c6cfb39e9c60efda1e43265428ff3c5e7bfde259
osa1 <omeragacan@gmail.com>
2013-05-29 13:15:04 +0300

    register set operations

--- main.c
+++ main.c
@@ -11,13 +11,41 @@ static int newset(lua_State *L)
 {
   lc_set *a = lua_newuserdata(L, sizeof(lc_set*));
   *a = lc_newset(L);
-  luaL_getmetatable(L, "Containers.set");
-  lua_setmetatable(L, -2);
+  /*luaL_getmetatable(L, "Containers.set");*/
+  /*lua_setmetatable(L, 1);*/
+  return 1;
+}
+
+static int set_count(lua_State *L)
+{
+  int ref = luaL_ref(L, LUA_REGISTRYINDEX);
+  /*lc_set *a = luaL_checkudata(L, 1, "Containers.set");*/
+  lc_set *a = lua_touserdata(L, 1);
+  int ret = lc_set_count(*a, ref);
+  lua_pushinteger(L, ret);
+  return 1;
+}
+
+static int set_insert(lua_State *L)
+{
+  int ref = luaL_ref(L, LUA_REGISTRYINDEX);
+  lc_set *a = lua_touserdata(L, 1);
+  lc_set_insert(*a, ref);
+  return 0;
+}
+
+static int set_size(lua_State *L)
+{
+  lc_set *a = lua_touserdata(L, 1);
+  lua_pushinteger(L, lc_set_size(*a));
   return 1;
 }
 
 static const struct luaL_Reg containerlib [] = {
   {"newset", newset},
+  {"set_count", set_count},
+  {"set_insert", set_insert},
+  {"set_size", set_size},
   {NULL, NULL}
 };
 
