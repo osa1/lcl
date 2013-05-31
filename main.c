013a780547937b966681fbb432a7bd19aaabf03d
osa1 <omeragacan@gmail.com>
2013-05-31 14:29:24 +0300

    stackdump function from PiL, for debugging purposes

--- main.c
+++ main.c
@@ -7,6 +7,41 @@
 #include "lualib.h"
 
 
+// used for debugging purposes
+static void stackdump(lua_State *L)
+{
+  int top = lua_gettop(L);
+  for (int i = 1; i <= top; i++)
+  {
+    int t = lua_type(L, i);
+    switch (t) {
+      case LUA_TSTRING:
+      {
+        printf("'%s'", lua_tostring(L, i));
+        break;
+      }
+      case LUA_TBOOLEAN:
+      {
+        printf(lua_toboolean(L, i) ? "true" : "false");
+        break;
+      }
+      case LUA_TNUMBER:
+      {
+        printf("%g", lua_tonumber(L, i));
+        break;
+      }
+      default:
+      {
+        printf("%s", lua_typename(L, t));
+        break;
+      }
+    }
+    printf("  ");
+  }
+  printf("\n");
+}
+
+
 static int set_new(lua_State *L)
 {
   int stack_depth = lua_gettop(L);
